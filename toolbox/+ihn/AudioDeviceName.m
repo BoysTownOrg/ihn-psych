36bee2edae437677981c60fff72b3e709542827c
Seth Bashford <seth.bashford@boystown.org>
2023-09-20 10:47:31 -0500

    add audio device names for meg

---
+++ toolbox/+ihn/AudioDeviceName.m
@@ -0,0 +1,7 @@
+classdef AudioDeviceName
+    properties (Constant)
+        meg1 = 'Digital Audio Interface (SPL Crimson)'
+        meg2 = 'Digital Audio Interface (USB Audio Device)'
+        bothMeg = 'Digital Audio Interface'
+    end
+end
