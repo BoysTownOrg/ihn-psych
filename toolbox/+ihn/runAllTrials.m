7abae5c48f8f2cc3b8c8a061f86a7812e11ef0b2
Seth Bashford <seth.bashford@boystown.org>
2024-03-12 14:50:32 -0500

    support pre/post functions and timeout when running all trials

--- toolbox/+ihn/runAllTrials.m
+++ toolbox/+ihn/runAllTrials.m
@@ -1,20 +1,7 @@
 function runAllTrials(trialFunction, trials, onHalfway)
-KbName('UnifyKeyNames');
-escapeKey = KbName('escape');
-% First call to KbCheck is slow
-[~, ~, ~] = KbCheck();
-
-priority = ihn.ScopedHighPriority; %#ok<NASGU>
-for i = 1:numel(trials)
-    trialFunction(trials(i));
-
-    [~, ~, keyCodes] = KbCheck;
-    if keyCodes(escapeKey)
-        return
-    end
-
-    if ceil(numel(trials) / 2) == i && nargin > 2
-        onHalfway();
-    end
+if nargin < 3
+    onHalfway = @()[];
 end
+ihn.runHighPriorityTask(trialFunction, trials,...
+    'halfwayFunction', onHalfway);
 end
---
+++ toolbox/+ihn/runHighPriorityTask.m
@@ -0,0 +1,33 @@
+function runHighPriorityTask(trialFunction, trials, options)
+    arguments
+        options.timeoutSeconds = inf
+        options.preFunction = @()[]
+        options.halfwayFunction = @()[]
+        options.postFunction = @()[]
+    end
+KbName('UnifyKeyNames');
+escapeKey = KbName('escape');
+% First call to KbCheck is slow
+[~, ~, ~] = KbCheck();
+
+priority = ihn.ScopedHighPriority; %#ok<NASGU>
+options.preFunction();
+s = GetSecs();
+for i = 1:numel(trials)
+    trialFunction(trials(i));
+
+    if GetSecs() - s > options.timeoutSeconds
+        break
+    end
+
+    [~, ~, keyCodes] = KbCheck;
+    if keyCodes(escapeKey)
+        return
+    end
+
+    if ceil(numel(trials) / 2) == i
+        options.halfwayFunction();
+    end
+end
+options.postFunction();
+end
