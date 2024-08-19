cec33fa423103371424436b733a973a3180a890a
Seth Bashford <seth.bashford@boystown.org>
2024-08-19 11:53:12 -0500

    breaking change; removing runAllTrials, should use runHighPriorityTask instead

--- toolbox/+ihn/runAllTrials.m
+++
@@ -1,7 +0,0 @@
-function runAllTrials(trialFunction, trials, onHalfway)
-if nargin < 3
-    onHalfway = @()[];
-end
-ihn.runHighPriorityTask(trialFunction, trials,...
-    'halfwayFunction', onHalfway);
-end
