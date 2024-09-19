46f17cdc0e9996adebb1305f7362a6a3c0960dd1
Seth Bashford <seth.bashford@boystown.org>
2024-09-19 09:52:22 -0500

    function to create output directory and check if file exists

---
+++ toolbox/+ihn/createWritableFile.m
@@ -0,0 +1,15 @@
+function file = createWritableFile(path, overwrite)
+if nargin < 2
+    overwrite = false;
+end
+directory = fileparts(path);
+[success, message] = mkdir(directory);
+if ~success
+    error('Failed to create output directory: %s: %s', directory, message);
+end
+if ~overwrite && exist(path, 'file') ~= 0
+    error('File already exists: %s', path);
+end
+
+file = ihn.WritableFile(outputFilePath);
+end
